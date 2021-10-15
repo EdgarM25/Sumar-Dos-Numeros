d322530a090904ee59bf723234f24307baf3276c
unknown <edgarmaldonado391@ujgh.edu.ve>
2021-10-14 21:55:52 -0400

    subiendo los archivos

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int numb1;
+    int numb2;
+    int result_of;
+printf("ingrese el primer valor\n");
+scanf("%d",&numb1);
+printf("ingrese el segundo valor\n");
+scanf("%d",&numb2);
+result_of = numb1 + numb2;
+printf("el resultado de la suma es:%d\n",result_of);
+return 0;
+}
